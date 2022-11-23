8ff293419e275417b757f3ced3c5fe09106c777a
Mathias Minor Büchler <MABC@simcorp.com>
2022-11-23 14:21:21 +0100

    Final code of assignment 1.

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -8,8 +8,32 @@ namespace homework2
 {
     class Program
     {
+        enum TransactionType
+        {
+            Buy = 1,
+            Sell = -1
+        }
+
         static void Main(string[] args)
         {
-        }
+
+            Console.WriteLine("Input the nominal of the trade");
+            var UInominal = Console.ReadLine();
+            var nominal = float.Parse(UInominal);
+
+            Console.WriteLine("Input the price of the trade");
+            var UIprice = Console.ReadLine();
+            float price = float.Parse(UInominal);
+
+            Console.WriteLine("Input the type of the trade");
+             
+            TransactionType trcType;
+            string userInput = Console.ReadLine();
+            trcType = (TransactionType) Enum.Parse(typeof(TransactionType), userInput,true);
+
+            double currentValue = ((double)trcType) * nominal * price;
+            
+            Console.WriteLine($"The current value of your investment is: {currentValue}");
+         }
     }
 }
